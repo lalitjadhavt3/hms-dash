6160e033e851521a0f91f6ede280fab5e1743173
lalij003 <lalit@wldigital.co.in>
2025-10-07 13:31:25 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Blank from "./pages/Blank";
 import AppLayout from "./layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
 import Home from "./pages/Dashboard/Home";
+import { hmsRoutes } from "./routes/hmsRoutes";
 
 export default function App() {
   return (
@@ -28,7 +29,7 @@ export default function App() {
           {/* Dashboard Layout */}
           <Route element={<AppLayout />}>
             <Route index path="/" element={<Home />} />
-            
+            {hmsRoutes}
             {/* Others Page */}
             <Route path="/profile" element={<UserProfiles />} />
             <Route path="/calendar" element={<Calendar />} />
